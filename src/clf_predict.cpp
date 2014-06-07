911cb453b51ce85b31d15ce6f960304a6112f999
Adam Martini <martini@cs.uoregon.edu>
2014-06-07 00:51:09 -0700

    Prediction workinggad  Need to fix off-by-one labeling again. Will do tomorrow.

--- src/clf_predict.cpp
+++ src/clf_predict.cpp
@@ -57,30 +57,31 @@ int main (int argc, char *argv[]) {
     // load testing data
     double feat_val, label;
 	for ( ProbSize i=0; i<m; ++i ) {
-	    std::ifstream data( datavec[i] );
+        //printf( "%s\n", datavec[i].c_str() );
+        std::ifstream data( datavec[i] );
 		for ( ProbSize j=0; j<n; ++j ) {
 			data >> feat_val;
-			printf( "feat_val %lf\n", feat_val );
+			//printf( "feat_val %lf\n", feat_val );
 			X(i,j) = feat_val;
 		}
 		data >> label;
 		labels[i] = label;
-		printf( "label %lf\n" label );
-        i++;
+		//printf( "label %lf\n", label );
 	}
-	std::cout << X << "\n" << labels << "\n";
+	//std::cout << X << "\n" << labels << "\n";
 
     // perform feature scaling (optional)
     if ( scaling ) {
 		mlu::scale_features( X, 1, 0 );
     }
-
+	std::cout << X << "\n";
 
 	/* FORMAT LABELS */
 	// format the local label set into a matrix based on global class map
+	mlu::get_unique_labels( labels, classmap );
 	Mat y = mlu::format_labels( labels, classmap );
 	numlabels = (LayerSize) classmap.size();
-
+    std::cout << y << "\n";
 
 	/* INIT LOCAL CLASSIFIER */
 	LogisticRegression clf( n, numlabels );
@@ -99,6 +100,7 @@ int main (int argc, char *argv[]) {
     for ( int i=0; i<num_params; ++i ) {
     	model >> theta[i];
     }
+    clf.set_theta( theta );
     std::cout << clf.get_theta() << "\n";
 
 
